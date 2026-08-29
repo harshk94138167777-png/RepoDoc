b97d8b144224ba338520a8e54c49183dec903107
harshk94138167777-png <harshk94138167777@gmail.com>
2026-08-30 02:13:17 +0530

    bump version to 1.0.1 to propagate README updates to PyPI

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="repodoctor-cli",
-    version="1.0.0",
+    version="1.0.1",
     description="Zero-dependency repository health analyser",
     long_description=long_description,
     long_description_content_type="text/markdown",
